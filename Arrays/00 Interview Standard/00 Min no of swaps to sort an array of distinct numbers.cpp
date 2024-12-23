0a7687218f9f3a0b8db0bb0befe07ebc9dbc82b9
Ashwani Yadav <ashyadavash@gmail.com>
2024-12-23 21:59:44 +0530

    Update 00 Min no of swaps to sort an array of distinct numbers.cpp

--- Arrays/00 Interview Standard/00 Min no of swaps to sort an array of distinct numbers.cpp
+++ Arrays/00 Interview Standard/00 Min no of swaps to sort an array of distinct numbers.cpp
@@ -2,6 +2,8 @@
 Que = Find min no of swaps required to sort an array consisting of distinct numbers
 */
 
+// NOTE: Array must have distinct elements for the below solutions.
+
 class Solution {
     public: int minSwaps(vector < int > & a) {
         int n = a.size();
@@ -19,5 +21,34 @@ class Solution {
             }
         }
         return swaps;
+    }
+};
+
+
+
+
+class Solution {
+    public: int minSwaps(vector < int > & a) {
+        int n = a.size();
+        vector < int > aSorted = a;
+        sort(aSorted.begin(), aSorted.end());
+
+        unordered_map < int, int > idxMap;
+        for (int i = 0; i < n; i++) {
+            idxMap[a[i]] = i;
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] == aSorted[i]) continue;
+            
+            cnt++;
+            int idx = idxMap[aSorted[i]];
+            swap(a[i], a[idx]);
+
+            idxMap[a[idx]] = idx;
+        }
+
+        return cnt;
     }
 };
